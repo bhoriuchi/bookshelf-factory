1e10ccd54c4c68ad4cb096f0d689f7d3b4aed4a4
Branden Horiuchi <bhoriuchi@gmail.com>
2015-08-26 15:24:29 -0700

    updating delete

---
+++ example/versioned/delete-test.js
@@ -0,0 +1,54 @@
+// create a database connection config
+var config = {
+	"client": "mysql",
+	"connection": {
+		"host": "127.0.0.1",
+		"user": "db",
+		"password": "password",
+		"database": "test",
+		"charset": "utf8"
+	},
+	debug: false
+};
+
+// import the modules
+var factory   = require('../../lib/factory')(config);
+var schema    = require('./versioned-schema')(factory.schemer.constants);
+var data      = require('./versioned-data');
+var models;
+
+// validate the schema
+schema = factory.prepareSchema(schema) || {};
+
+
+//console.log(schema);
+//process.exit();
+
+// forge all of the model definitions
+models = factory.create(schema);
+
+//console.log(models._relations);
+
+
+return models.list.forge().view()
+//.search('shopping')
+.deleteResource(1, {force: true})
+.end()
+.then(function(results) {
+	console.log(JSON.stringify(results, null, '  '));
+})
+.then(function() {
+	// exit the app
+	process.exit();
+});
+
+
+
+
+
+
+
+
+
+
+
